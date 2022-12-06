99eb95a020261ba47e5b453c72b01f35257bcc29
Guillermo Muntaner <guillermomp87@gmail.com>
2022-12-06 09:38:08 +0100

    Day 06

--- src/day06.rs
+++ src/day06.rs
@@ -1,9 +1,27 @@
-pub fn part1(_input: &str) -> Option<usize> {
-    None
+use std::collections::HashSet;
+
+pub fn start_of_message(input: &str, lenght: usize) -> usize {
+    input
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(lenght)
+        .enumerate()
+        .find_map(|(index, window)| {
+            if HashSet::<&char>::from_iter(window).len() == lenght {
+                Some(index + lenght)
+            } else {
+                None
+            }
+        })
+        .unwrap()
 }
 
-pub fn part2(_input: &str) -> Option<usize> {
-    None
+pub fn part1(input: &str) -> Option<usize> {
+    start_of_message(input, 4).into()
+}
+
+pub fn part2(input: &str) -> Option<usize> {
+    start_of_message(input, 14).into()
 }
 
 #[cfg(test)]
@@ -13,12 +31,24 @@ mod tests {
     const DAY: usize = 6;
 
     #[test]
+    fn test_part1_example1() {
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        assert_eq!(super::part1(input), Some(7));
+    }
+
+    #[test]
     fn test_part1() {
-        assert_eq!(super::part1(&utils::read_input(DAY)), None);
+        assert_eq!(super::part1(&utils::read_input(DAY)), Some(1275));
+    }
+
+    #[test]
+    fn test_part1_example2() {
+        let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+        assert_eq!(super::part2(input), Some(19));
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(super::part2(&utils::read_input(DAY)), None);
+        assert_eq!(super::part2(&utils::read_input(DAY)), Some(3605));
     }
 }
